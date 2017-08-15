e698ddb6c8e2e6cced7fb0990ffb81a79ad9a4b1
Sanjay Soni <sanjaysoniji@outlook.com>
2017-08-16 00:11:14 +0530

    Tiles View With All Models

---
+++ AspNetMVCSandbox/Controllers/DashboardController.cs
@@ -0,0 +1,22 @@
+﻿using AspNetMVCSandbox.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMVCSandbox.Controllers
+{
+    public class DashboardController : Controller
+    {
+        NorthwindDbContext dbContext = new NorthwindDbContext();
+        // GET: Dashboard
+        public ActionResult Index(string customerID)
+        {
+            //if(String.IsNullOrEmpty(customerID))
+            //var customers = dbContext.CustOrderHist(customerID).ToList();
+            var customers = dbContext.CustOrderHist("ALFKI").ToList();
+            return View(customers);
+        }
+    }
+}
